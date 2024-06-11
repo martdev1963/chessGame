2491d50eb215e38766e08f8b6705d97c880bd2d7
unknown <martdev1963@gmail.com>
2024-06-11 17:21:43 +0100

    vid_time: 7:05

---
+++ app.js
@@ -0,0 +1,10 @@
+// object declarations...
+const gameBoard = document.querySelector("#gameboard");
+const playerDisplay = document.querySelector("#player");
+const infoDIsplay = document.querySelector("#info-display");
+const width = 8;
+
+
+const startPieces = [];
+
+
